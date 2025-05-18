54b77e764ac8e1774d56326be26e978f671ed303
b-k-a <ben-alcorn@tutanota.com>
2025-05-17 19:23:30 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, entry):
+  _LOGGER.debug("Forwarding LDS config entry to sensor platform")
+  hass.async_create_task(
+    hass.config_entries.async_forward_entry_setup(entry, "sensor")
+  )
+  return True
+
+async def async_unload_entry(hass, entry):
+  return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
