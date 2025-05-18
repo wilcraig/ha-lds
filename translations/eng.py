3382505a870794b8f457546ebc7c2697b6aebc95
b-k-a <ben-alcorn@tutanota.com>
2025-05-17 19:56:47 -0500

    Create eng.py

---
+++ translations/eng.py
@@ -0,0 +1,14 @@
+{
+  "config": {
+    "step": {
+      "user": {
+        "title": "Configure LDS Scripture Sensor",
+        "description": "Choose language and resource type",
+        "data": {
+          "language": "Language (e.g. eng, spa, ...)",
+          "resource": "Resource Type"
+        }
+      }
+    }
+  }
+}
