958fb89fe9ec572b7804a96415c544594608ff83
b-k-a <ben-alcorn@tutanota.com>
2025-05-17 20:32:15 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,10 +5,16 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry):
   _LOGGER.debug("Forwarding LDS config entry to sensor platform")
+
+  hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+  
   hass.async_create_task(
     hass.config_entries.async_forward_entry_setup(entry, "sensor")
   )
   return True
 
 async def async_unload_entry(hass, entry):
-  return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+  unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+  if unload_ok:
+    hass.data[DOMAIN].pop(entry.entry_id)
+  return unload_ok
